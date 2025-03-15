cdd32d55d01a7ddbb34f82bcbb6490e52a2f279e
iloniemi <juho.iloniemi@gmail.com>
2025-03-15 18:04:54 +0200

    9.14

--- backend-patientor/src/utils.ts
+++ backend-patientor/src/utils.ts
@@ -1,61 +1,16 @@
 import { NewPatient, Gender } from './types';
+import { z } from 'zod';
 
 const toNewPatient = (object: unknown): NewPatient => {
-  if (!object || typeof object !== 'object') {
-    throw new Error('No data');
-  }
-  if ('name' in object && 'dateOfBirth' in object
-    && 'ssn' in object  && 'gender' in object && 'occupation' in object) {
-
-      const newPatient: NewPatient = {
-        name: parseName(object.name),
-        dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-        ssn: parseSsn(object.ssn),
-        gender: parseGender(object.gender),
-        occupation: parseOccupation(object.occupation),
-    };
-      return newPatient;
-    }
-  throw new Error('Incorrect data: some fields are missing');
+  return newPatientSchema.parse(object);
 };
 
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender).map(v => v.toString()).includes(param);
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!isString(gender) || !isGender(gender)) throw new Error('Incorrect or missing gender');
-  return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!isString(occupation)) throw new Error('Incorrect or missing occupation');
-  return occupation;
-};
-
-const parseSsn = (ssn: unknown): string => {
-  if (!isString(ssn)) throw new Error('Incorrect or missing ssn');
-  return ssn;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDateOfBirth = (date: unknown): string => {
-  if (!isString(date) || !isDate(date)) throw new Error('Incorrect or missing date');
-  return date;
-};
-
-const parseName = (name: unknown): string => {
-  if (!isString(name)) {
-    throw new Error('Incorrect or missing name');
-  }
-  return name;
-};
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
+export const newPatientSchema = z.object({
+  gender: z.nativeEnum(Gender),
+  dateOfBirth: z.string().date(),
+  ssn: z.string(),
+  occupation: z.string(),
+  name: z.string(),
+});
 
 export default toNewPatient;
